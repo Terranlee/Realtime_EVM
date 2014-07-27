962633bba1a780b688874f90f790a76235e77ad6
Terranlee <ltrthu@163.com>
2014-07-28 00:55:41 +0800

    add more exceptions using derived class

--- README.md
+++ README.md
@@ -1,5 +1,11 @@
-This is a program about Eulerian Video Magnification
-This program provides realtime motion magnification and frequency analysis of the motion
-The algorithm is mostly based on this paper
-	http://people.csail.mit.edu/mrub/vidmag/
-Use openCV for image processing and Qt for showing the result
+Realtime Eulerian Video Magnification
+-------------------------------------------------
+This is a program about Eulerian Video Magnification   
+This program provides realtime motion magnification and frequency analysis of the motion     
+The algorithm is mostly based on this paper   
+	http://people.csail.mit.edu/mrub/vidmag/    
+
+> Platform and labraries:
+> > openCV2.4.2 for linux
+> > Qt5.1.0 for linux
+
--- error.h
+++ error.h
@@ -1,11 +1,64 @@
 #ifndef ERROR_H
 #define ERROR_H
 
+#include <iostream>
+#include <QString>
+
 namespace error
 {
-    struct NoFace
+    class MessageError
+	{
+    private:
+        QString message;
+    public:
+        MessageError(QString m) : message(m) {}
+        QString print() { return message; }
+	};
+
+    struct NoFaceError
     {
 
+    };
+
+    //errors related to numbers
+    class NumberError
+    {
+    protected:
+        QString which;        //which arg is it?
+        float args;           //what is the arg?
+    public:
+        NumberError(QString w , float a ) : which(w) , args(a) {}
+        virtual QString print_message() = 0;
+    };
+
+    //when some parameters are negative , public derived from NumberError
+    class NegativeError : public NumberError
+    {
+    public:
+        NegativeError(QString w , float a) : NumberError(w , a) {}
+        virtual QString print_message(){
+            return QString("number ") + QString("%1").arg(args) + QString("of ") + which + QString("is negative");
+        }
+    };
+
+    //when some parameters are too large
+    class TooLargeError : public NumberError
+    {
+    public:
+        TooLargeError(QString w , float a) : NumberError(w , a) {}
+        virtual QString print_message(){
+            return QString("number ") + QString("%1").arg(args) + QString("of ") + which + QString("is too large");
+        }
+    };
+
+    //when some parameters are too small
+    class TooSmallError : public NumberError
+    {
+    public:
+        TooSmallError(QString w , float a) : NumberError(w , a) {}
+        virtual QString print_message(){
+            return QString("number ") + QString("%1").arg(args) + QString("of ") + which + QString("is too small");
+        }
     };
 }
 
